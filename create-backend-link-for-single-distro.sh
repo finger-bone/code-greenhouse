4d24a9f740edd9cd97068e134a582e8e99f26b0b
Zend <zendwang040302@outlook.com>
2024-11-01 11:33:17 +0800

    fix softlink

--- create-backend-link-for-single-distro.sh
+++ create-backend-link-for-single-distro.sh
@@ -1,9 +1,6 @@
-mkdir -p single-distro
-for file in backend/*; do
-  if [[ $(basename "$file") != "main.go" && $(basename "$file") != "config.toml" ]]; then
-    target="single-distro/$(basename "$file")"
-    if [[ ! -e "$target" ]]; then
-      ln -s "$(realpath "$file")" "$target"
-    fi
+cd backend/
+for item in *; do
+  if [[ "$item" != "main.go" && "$item" != "config.toml" ]]; then
+    ln -s "$PWD/$item" "../single-distro/$item"
   fi
 done
